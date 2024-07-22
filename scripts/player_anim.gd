d252af12654a2ce3cf353e83b2ee65aac14a206b
Lucas-Animax <lucaslemos1402@gmail.com>
2024-07-22 11:19:22 -0300

    current moving player
    -creating moving to player

---
+++ scripts/player_anim.gd
@@ -0,0 +1,43 @@
+extends Sprite2D
+
+@export_category("variebles")
+@export_category("Objects")
+@export var player_ref:CharacterBody2D
+@export var anim:AnimationPlayer
+
+
+func animate(dic:float,dicV2:Vector2):
+	if dicV2.y != 0:
+		anim_vertical(dicV2)
+	elif dicV2.x != 0:
+		anim_horizontal(dic)
+	check_side(dic)
+
+func check_side(dic:float):
+	if dic == 1:
+		player_ref.direction = -1
+		flip_h = false
+		player_ref.wall_ray.target_position = Vector2(10,0)
+	elif dic == -1:
+		player_ref.direction = 1
+		flip_h = true
+		player_ref.wall_ray.target_position = Vector2(-10,0)
+
+func anim_horizontal(dic):
+	if dic != 0:
+		anim.play("walk")
+
+func anim_vertical(dicV2:Vector2):
+	if player_ref.next_to_wall() and not player_ref.is_on_floor():
+		anim.play("wall_slide")
+	elif dicV2.y < 0:
+		anim.play("up")
+	elif dicV2.y > 0:
+		anim.play("down")
+	pass 
+
+
+
+
+
+
