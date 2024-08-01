9d532e2e1b985300ed92370915b2ebc1ff638061
Lucas-Animax <lucaslemos1402@gmail.com>
2024-08-01 13:20:50 -0300

    button in word1
    -update in interfase

--- scripts/runs_pass.gd
+++ scripts/runs_pass.gd
@@ -2,7 +2,20 @@ extends TouchScreenButton
 
 @export var current_level:int
 @export var need_keys:int
+@export var anim:AnimationPlayer
+
+func _ready():
+	anim_button()
+	pass
 
 func _on_pressed():
-	if need_keys <= find_iten.current_keys :
+	if need_keys <= find_iten.current_keys:
+		anim.play("click")
+		await get_tree().create_timer(0.8).timeout
 		get_tree().change_scene_to_file(WorldFaze.worlds[current_level])
+
+func anim_button():
+	if need_keys <= find_iten.current_keys:
+		anim.play("onn")
+	else: 
+		anim.play("off")
